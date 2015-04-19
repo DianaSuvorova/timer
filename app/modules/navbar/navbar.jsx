7ec9c8e0e608062aa7756bb4f834c6bfe164ab57
diana suvorova <diana.suvorova@ayasdi.com>
2015-04-19 14:47:02 -0700

    navbar action and styling

--- app/modules/navbar/navbar.jsx
+++ app/modules/navbar/navbar.jsx
@@ -1,13 +1,17 @@
 Timer.Views.Navbar = React.createClass({
+  //When profile is needed <div className = 'profile' ><div>Sign In</div><div>Register</div></div>
+
   render: function () {
+    var navbarClass = classNames ({
+      'navbar': true,
+      'dark' : !this.props.currentPageTimer,
+      'light' : this.props.currentPageTimer 
+    }); 
+
     return (
-      <div className = 'navbar'>
-        <div className = 'timer'> Timer </div>
-        <div className = 'routines'> Daily Routines </div>
-        <div className = 'profile' >
-          <div>Sign In</div>
-          <div>Register</div>
-        </div>
+      <div className = {navbarClass} >
+        <div className = 'timer' onClick = {function () {this.props.setTimerCurrentPage(true); }.bind(this)}> Timer </div>
+        <div className = 'routines' onClick = {function () {this.props.setTimerCurrentPage(false); }.bind(this)}> Daily Routines </div>
       </div>
     );
   }
