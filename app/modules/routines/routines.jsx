064d92e71c2b768781afdffd990e4879db4b0549
diana suvorova <diana.suvorova@ayasdi.com>
2015-04-19 15:26:29 -0700

    routines page mock

--- app/modules/routines/routines.jsx
+++ app/modules/routines/routines.jsx
@@ -1,5 +1,16 @@
 Timer.Views.Routines = React.createClass({
   render: function () {
-    return <div id = 'routines'> Routines </div>;
+
+    var content = ' I write my stories in the morning my diariy at night. There is interruption all the time but I can quickly deal with an interruption and then know that its Tuesday, I have product meetings and I need to focus on product stuff.';
+
+    return (
+      <div id = 'routines'>  
+        <div className = 'navigation left'><div><i className="fa fa-long-arrow-left"></i></div></div>
+        <div className= 'content'>
+          <div>{content}</div>
+        </div>
+        <div className = 'navigation right'><div><i className="fa fa-long-arrow-right"></i></div></div>
+      </div>
+    );
   }
 });
