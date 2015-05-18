5f1237e97e0ddb91245253378d1ca1648d97b7a5
diana suvorova <diana.suvorova@ayasdi.com>
2015-05-17 19:12:04 -0700

    profile and register form

--- app/modules/navbar/navbar.react.js
+++ app/modules/navbar/navbar.react.js
@@ -1,7 +1,7 @@
 var Link = Router.Link;
+var Profile = require('./../profile/profile.react');
 
 var navbar = React.createClass({
-  //When profile is needed <div className = 'profile' ><div>Sign In</div><div>Register</div></div>
 
   render: function () {
 
@@ -9,6 +9,7 @@ var navbar = React.createClass({
       <div id = 'navbar' className = {this.props.className} >
         <Link to='timer'>Timer</Link>
         <Link to='routines'>Daily Routines</Link>
+        <Profile/>
       </div>
     );
   }
---
+++ app/modules/profile/profile.react.js
@@ -0,0 +1,56 @@
+var profile = React.createClass({
+  getInitialState: function () {
+    return {signIn: false, register: false};
+  },
+
+  toggleSignIn: function () {
+    this.setState({signIn: !this.state.signIn});
+  },
+
+  toggleRegister: function () {
+    this.setState({register: !this.state.register});
+  },
+
+  register: function (e) {
+    e.preventDefault();
+    var username = $('#register').find('input.username').val();
+    var password = $('#register').find('input.password').val();
+
+    var user = new Parse.User();
+    user.set('username', username);
+    user.set('password', password);
+
+    user.signUp(null, {
+      success: function(user) {
+        console.log('User "' + username + '" successfully created!');
+      },
+      error: function(user, error) {
+        console.warn("Error: " + error.code + " " + error.message);
+      }
+    });
+  },
+
+  render: function () {
+    signInClass = ClassNames({'expanded': this.state.signIn });
+    registerClass = ClassNames({'expanded': this.state.register });
+
+    return (
+      <div id = 'profile'>
+        <div className = {signInClass}>
+          <span onClick = {this.toggleSignIn}> Sign In </span>
+        </div>
+        <div className = {registerClass}>
+          <span onClick = {this.toggleRegister}>Register</span>
+          <form id = 'register'>
+            <input className = 'username' type = 'text' placeholder = 'Username'/>
+            <input className = 'password' type = 'password' placeholder = 'Password'/>
+            <input type = 'password' placeholder = 'Confirm password'/>
+            <button onClick = {this.register} >REGISTER</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports =  profile;
